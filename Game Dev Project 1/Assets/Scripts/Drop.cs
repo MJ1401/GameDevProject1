165a6e2b18440885fe0710540e09dc51288eeebb
MJ1401 <johnsonmb@hendrix.edu>
2023-01-31 10:03:11 -0600

    Tried to Add Dropping (but failed)
    There is a bug in the C Script that isn't making the script work as indented. It is supposed to drop a diamond every so often but isn't. Error: variable diamond of Drop has not been assigned.

---
+++ Game Dev Project 1/Assets/Scripts/Drop.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drop : MonoBehaviour
+{
+
+    public GameObject diamond;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("DropDiamond", 2.0f, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DropDiamond() {
+        Instantiate(diamond);
+    }
+}
